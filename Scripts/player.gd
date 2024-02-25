97ba7b09f411d94aec6be1ee2e4da42c2f7768fc
Emily Krohn <emilykrohn2@gmail.com>
2024-02-25 06:58:21 -0800

    Add player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
+var speed := 100
+var jump_force := 500
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		global_position.x -= speed * delta
+	elif Input.is_action_pressed("right"):
+		global_position.x += speed * delta
+	elif Input.is_action_pressed("up"):
+		global_position.y -= jump_force * delta
